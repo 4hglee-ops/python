0ee19c241df55dbc206701fb896a9f8733bc6f36
4hglee-ops <4hglee@gmail.com>
2026-07-18 02:49:48 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ Python3/프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -2,13 +2,18 @@ def solution(s):
     answer = ''
     s_dict = {}
     for text in s:
-        s_dict[text]=s_dict.get(text,0)
-        if s_dict[text]==0:
-            s_dict[text] = 1
-        else:
-            s_dict[text]+=1
+        s_dict[text]=s_dict.get(text,0)+1
     for key in s_dict.keys():
         if s_dict[key] == 1:
             answer += key
-        answer = "".join(sorted(answer))
+    answer = "".join(sorted(answer))
     return answer
+
+# def solution(s):
+#     answer = []
+#     set_s = set(s)
+#     for text in set_s:
+#         if s.count(text)==1:
+#             answer.append(text)
+#     answer = "".join(sorted(answer))
+#     return answer
