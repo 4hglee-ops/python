b17631476a9e026bfa068fc3b1d08dd5c392ac84
4hglee-ops <4hglee@gmail.com>
2026-07-17 21:25:14 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,13 @@
+def solution(age):
+    answer = ''
+    age_dict = {
+        0:'a',1:'b',2:'c',3:'d',4:'e',
+        5:'f',6:'g',7:'h',8:'i',9:'j'
+    }
+    age_str = str(age)
+    
+    for text in age_str:
+        answer+=age_dict[int(text)]
+    
+    
+    return answer
