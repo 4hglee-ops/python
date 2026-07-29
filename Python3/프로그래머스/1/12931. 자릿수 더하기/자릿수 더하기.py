0c9b82db70a5bcf45d390e0d79e8e98f84a0b48a
Honggyu Lee <4hglee@gmail.com>
2026-07-30 06:08:50 +0900

    Update 자릿수 더하기.py

--- Python3/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ Python3/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,12 +1,10 @@
 def solution(n):
     answer = 0
     n_range = len(str(n))-1
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print(n_range)
+
     for i in range(n_range,-1,-1):
         answer += n // (10 ** i)
         n = n % (10**i)
         print(i,10**i,answer,n)
-    
     
     return answer
